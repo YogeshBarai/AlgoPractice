b724ff8be9711e0b4b0379cedb5c7685ee2c88bc
YogeshBarai <70328058+YogeshBarai@users.noreply.github.com>
2023-01-14 11:15:00 +0530

    Refactor code

--- ShortestDistance.py
+++ ShortestDistance.py
@@ -2,83 +2,37 @@
 # %% IMPORTS
 from sys import maxsize
 
-# %% INITIALIZE
-blocks = [
-    {
-        "gym": False,
-        "school": True,
-        "store": False
-    },
-    {
-        "gym": True,
-        "school": False,
-        "store": False
-    },
-    {
-        "gym": True,
-        "school": True,
-        "store": False
-    },
-    {
-        "gym": False,
-        "school": True,
-        "store": False
-    },
-    {
-        "gym": False,
-        "school": True,
-        "store": True
-    }
-]
+blocks = [{'gym': False, 'school': True, 'store': False},
+          {'gym': True, 'school': False, 'store': False},
+          {'gym': True, 'school': True, 'store': False},
+          {'gym': False, 'school': True, 'store': False},
+          {'gym': False, 'school': True, 'store': True}]
 
-# %% REQUIREMENTS
-reqs = ['gym','store','school']
+reqs = ['gym', 'school', 'store']
 
-# %% LOGIC
-distances=[]
-distance_dict={}
-min_distance=maxsize
+total_blocks = len(blocks)
+
+min_distance = maxsize
 best_block = -1
-for index, block in enumerate(blocks):
-    # Search current block
-    for req in reqs:
-        if req in block.keys():
-            if block[req]:
-                distance_dict[req+'dist'] = 0
+
+for current_block_index in range(total_blocks-1):
+    for next_block_index in range(current_block_index+1, total_blocks):
+        next_block = blocks[next_block_index]
+        current_block = blocks[current_block_index]
+
+        dist = 0
+        for req in reqs:
+            if next_block[req] == current_block[req]:
+                dist += 0
             else:
-                distance_dict[req+'dist'] = 100
-                
-    #Traverse Previous blocks
-    for previous_block in range(0, index):
-        for req in reqs:
-            if req in blocks[previous_block].keys():
-                if blocks[previous_block][req]:
-                    if req+'dist' in distance_dict.keys():
-                        if distance_dict[req+'dist'] == 0: 
-                            distance = 0
-                        elif distance_dict[req+'dist'] == 100:
-                            distance = previous_block-index
-                        else:
-                            distance = max(distance_dict[req+'dist'], previous_block-index)
+                dist += abs(next_block_index-current_block_index)
 
-                        distance_dict[req+'dist'] = distance
-    
-    #Traverse Next blocks
-    for next_block in range(index+1, len(blocks)):
-        for req in reqs:
-            if req in blocks[next_block].keys():
-                if blocks[next_block][req]:
-                    if req+'dist' in distance_dict.keys():
-                        if distance_dict[req+'dist'] == 0: 
-                            distance = 0
-                        elif distance_dict[req+'dist'] == 100:
-                            distance = index-next_block
-                        else:
-                            distance = min(distance_dict[req+'dist'], previous_block-index)
+        if dist < min_distance:
+            min_distance = dist
+            best_block = next_block_index
 
-                        distance_dict[req+'dist'] = distance
+        print(f"\t({current_block_index}, {next_block_index}):\t{dist}")
+        print(f"\t({best_block}):\t{min_distance}")
 
-    distances.append(distance_dict.copy())
-    
-print(distances)
-  
+print(f"Best block index: {best_block}")
+# %%
